8d5bebadfc4860499c1b2ac89dfa90bd46f9194c
Daniel C. Landon Jr <daniel.landonjr@gmail.com>
2018-08-29 05:38:43 -0400

    Demonstrate how to add css in various fashions, inline etc...

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// can include external css file and css will be applied as normal but only to this component
+// import './CSS_FILE_NAME.css';
+
 // include props here and you can pass in properties from the calling app
 const Header = props => {
   // destructure props to get the parts that we want
   const { branding } = props;
   return (
     <div>
-      <h1>{branding}</h1>
+      {/* <h1>{branding}</h1> */}
+
+      {/* inline style */}
+      {/* <h1 style={{ color: 'red', fontSize: '50px' }}> {branding}</h1> */}
+
+      {/* included as varialbe declared below */}
+      <h1 style={headingStyle}> {branding}</h1>
     </div>
   );
 };
@@ -25,6 +34,11 @@ Header.defaultProps = {
 Header.propTypes = {
   // declare that branding is a string and is required
   branding: PropTypes.string.isRequired
+};
+
+const headingStyle = {
+  color: 'red',
+  fontSize: '50px'
 };
 
 export default Header;
